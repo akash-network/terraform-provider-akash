33476832db6cb46a50c97abf3cbfb4e658527570
Joao Luna <7607329+cloud-j-luna@users.noreply.github.com>
2022-07-31 23:32:52 +0100

    Hotfix of provider configuration and username change (#13)

--- examples/main.tf
+++ examples/main.tf
@@ -5,19 +5,13 @@
 terraform {
   required_providers {
     akash = {
-      version = "0.0.2"
-      source  = "joao-luna-98/akash"
+      version = "0.0.3"
+      source  = "cloud-j-luna/akash"
     }
   }
 }
 
 provider "akash" {
-  account_address = "akash1qyfg4zl2dku8ry7gjkhf88vnc3zrn6vmnzlvr9"
-  keyring_backend = "os"
-  key_name = "terraform"
-  node = "http://akash.c29r3.xyz:80/rpc"
-  chain_id = "akashnet-2"
-  chain_version = "0.16.4"
 }
 
 resource "akash_deployment" "my_deployment" {
