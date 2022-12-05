8903e6ec7ac5f8e8bebc512b0f6649ef53b0fcd6
Joao Luna <joaopedroluna98@gmail.com>
2022-12-05 15:49:16 +0000

    Progress on deployment output

--- akash/client/types/lease_status.go
+++ akash/client/types/lease_status.go
@@ -1,27 +1,7 @@
 package types
 
-import "net"
+import (
+	akashtypes "github.com/ovrclk/provider-services/gateway/rest"
+)
 
-type LeaseStatus struct {
-	Services       map[string]Service `json:"services"`
-	ForwardedPorts []ForwardedPort    `json:"forwarded_ports"`
-	IPs            map[string]IP      `json:"ips"`
-}
-
-type IP struct {
-	Address net.IP `json:"address"`
-}
-
-type ForwardedPort struct {
-}
-
-type Service struct {
-	URIs              []string `json:"uris"`
-	Name              string   `json:"name"`
-	Available         int32    `json:"available"`
-	Total             int32    `json:"total"`
-	Replicas          int32    `json:"replicas"`
-	UpdatedReplicas   int32    `json:"updated_replicas"`
-	ReadyReplicas     int32    `json:"ready_replicas"`
-	AvailableReplicas int32    `json:"available_replicas"`
-}
+type LeaseStatus akashtypes.LeaseStatus
